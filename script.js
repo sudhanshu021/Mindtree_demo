41f394acee89f0df334498ef48cfc79cb5ba69f6
sudhanshu singh <63348165+sudhanshu021@users.noreply.github.com>
2023-04-17 17:20:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var a="sam";
+let b="jane";
+const c="william";
+if(true)
+{
+    var a="saming";
+    let b="janeing";
+    const c="williaming";
+    console.log(a);
+    console.log(b);
+    console.log(c);
+
+}
+var a="harry";
+console.log(a);
+console.log(b);
+console.log(c);
